53fc8e460d17c97eecbf83b1e1d41afff58d486a
zephyrus21 <pandey.piyus@gmail.com>
2021-02-23 19:03:03 +0530

    User class defined

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Piyush', age: 20 });
-
-console.log(user.get('name'));
-
-user.set({ name: 'Neha', age: 19 });
-
-console.log(user.get('name'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,8 +3,13 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
   constructor(private data: UserProps) {}
+
+  //! we will have array of events and each event will also have array of callback functions
+  events: { [key: string]: Callback[] } = {};
 
   get(propName: string): string | number {
     return this.data[propName];
@@ -12,5 +17,23 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
