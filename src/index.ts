d7c48090e84e86bd557e181d3f41e130b50e2a7d
zephyrus21 <pandey.piyus@gmail.com>
2021-02-25 17:37:19 +0530

    up

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { UserForm } from './views/UserForm';
 
 const user = User.build({ name: 'test', age: 99 });
 
-const userForm = new UserForm(document.getElementById('root'), user);
+const root = document.getElementById('root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found!');
+}
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,16 +1,29 @@
 import { User } from '../models/User';
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.change-name': this.onChangeNameClick,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onChangeNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
   };
 
   template(): string {
@@ -20,6 +33,7 @@ export class UserForm {
         <div>NAME: ${this.model.get('name')}</div>
         <div>AGE: ${this.model.get('age')}</div>
         <input/>
+        <button class="change-name">Update Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `;
@@ -38,6 +52,8 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
