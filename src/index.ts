bd1b3101c5a1c7b5b535b7a89cabef3e7bac8aed
zephyrus21 <pandey.piyus@gmail.com>
2021-02-27 19:59:45 +0530

    adding UserList

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import { User } from './models/User';
-import { UserEdit } from './views/UserEdit';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
 
-const user = User.build({ name: 'test', age: 99 });
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.build(json);
+  }
+);
 
-const root = document.getElementById('root');
+users.on('change', () => {
+  const root = document.querySelector('#root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found!');
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
---
+++ src/views/CollectionView.ts
@@ -0,0 +1,21 @@
+import { Collection } from '../models/Collection';
+
+export abstract class CollectionView<T, K> {
+  constructor(public parent: Element, public collection: Collection<T, K>) {}
+
+  abstract renderItem(model: T, itemParent: Element): void;
+
+  render(): void {
+    this.parent.innerHTML = '';
+
+    const templateElement = document.createElement('template');
+
+    for (let model of this.collection.models) {
+      const wrapper = document.createElement('div');
+      this.renderItem(model, wrapper);
+      templateElement.content.appendChild(wrapper);
+    }
+
+    this.parent.appendChild(templateElement.content);
+  }
+}
